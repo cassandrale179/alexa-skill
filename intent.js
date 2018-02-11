33ed93a0e403c63537af93f3acbe91b2f0894d00
Cassandra Le <cassandrale@Cassandras-MBP.fios-router.home>
2018-02-10 21:03:18 -0500

    DONE: add intent and utterance

---
+++ intent.js
@@ -0,0 +1,30 @@
+//Intent Schema
+{
+    "intents": [
+        {
+            "intent": "GetUserSongTags"
+        },
+        {
+            "intent": "PlayMusic"
+        }
+    ]
+}
+
+
+//Sample Utterances
+//Intent name: phrase to trigger intent
+
+
+/*
+
+PlayMusic play music
+PlayMusic can you play me some music
+PlayMusic can you give me some song
+PlayMusic I am sad can you give me some song
+PlayMusic DJ turn it up
+PlayMusic play me something
+
+
+
+
+*/
