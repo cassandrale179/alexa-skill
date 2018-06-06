fe23a494f86be3009e46734718dbe4369e95405b
Minh Le <lenguyetminh1998@gmail.com>
2018-06-06 16:25:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,82 +1,78 @@
-const AWS = require('aws-sdk');
-const proxy = require('proxy-agent');
-const bucketname = 'cassandrale19-bms-central-us-east-1'; 
-const bucketkey = 'doAtTrack/callback.json'; 
+var AWS = require('aws-sdk');
+
+exports.myHandler = function (event, context, callback) {
+	console.log("Hello server! event is ", event);
+	console.log("Hello server! context is ", context);
 
 
-//-------- SPECIFY PARAMETERS ON WINDOWS --------
-AWS.config.update({region: 'us-west-2'});
+	// Set bucket name and bucket key of S3 here
+	var s3 = new AWS.S3();
+	var bucketname = 'doatbucket-lem1';
+	var bucketkey = 'doAtTrack/endpoint1.json';
 
-
-//-------- CREATING AWS RESOURCES ---------
-const s3 = new AWS.S3();
-const lambda = new AWS.Lambda();
-
-
-
-
-//--------- EXPORT HANDLER TO WRITE TO S3 BUCKET ---------
-exports.handler = function(event, context, callback) {
-    console.log("This is the event", event);
-    console.log("This is the context", context);
-    console.log("This is the s3 bucket", s3); 
-    
-    //----- LIST OF VARIABLES TO BE USED ------- 
-    var bucketParams = {
+	// Create bucket params over here
+	var bucketParams = {
         Bucket: bucketname,
         Key: bucketkey,
     };
+
+	// Data to be written to the bucket
     var fakedata = {
-    	"triggerDate": "2018-02-06T21:54:32Z",
-    	"payload": "test sns message",
-    	"callbackType": "sns",
-    	"callbackEndpoint": "arn:aws:sns:us-east-1:315327487940:doAtSNSGuide_us-east-1",
-    	"requesterEmail": "minh.le@bms.com"
+        message: "hello world",
+    	callbackEndpoint: "endpoint1",
+    	requesterEmail: "minh.le@bms.com",
+		executed: "true"
     };
 
 
-    //--------- GET OBJECT TAGS AND EXECUTION ID OF LAMBDA --------
+	/**
+		@defintion: this function get tags of an object
+	*/
     function getObjectTagging(){
         s3.getObjectTagging(bucketParams, function(err, data){
 
             // If no object exist yet, create one
             if (err){
                 if (err.code == 'NoSuchKey'){
-                   uploadFile(fakedata);  
+                   uploadFile(fakedata);
                 }
-                    
+
+				// If error is not NoSuchKey, that probably means access denied
                 else{
                   console.error("[Error at line 37: ] Unknown error", err);
+                  uploadFile(fakedata);
                 }
             }
-            
-            //If object already exist, check for the tags 
+
+            // If object already exist, check for the tags
             else{
-                var tag = data.TagSet.filter(obj => obj.Key == 'claim')[0]; 
+                var tag = data.TagSet.filter(obj => obj.Key == 'claim')[0];
                 if (tag){
 
-                    //If another Lambda is writing to it, sleep for 1000 ms
+                    // If another Lambda is writing to it, sleep for 1000 ms
                     if (tag.Value != context.invokedFunctionArn && tag.Value != ''){
                         console.log('Another lambda is writing to this file.');
                         setTimeout(getObjectTagging, 1000);
                     }
 
-                    //If the tags are your own tags, then writing to it
+                    // If the tags are your own tags, then you can write to it
                     else{
                         console.log("Can write to bucket");
-                        appendContent(); 
+                        appendContent();
                     }
                 }
             }
         });
     }
-    
-    
-    //-------- APPEND NEW CONTENT TO THE CALLBACK.JSON ------ 
+
+
+	/**
+		@param {string} uploadContent: this string is written to the .json file
+	*/
     function appendContent(){
         s3.getObject(bucketParams, function(err, data) {
 
-           //--- Create object if none exist in bucket ---
+           // Create object if none exist in bucket ---
            if (err){
                if (err.code == 'NoSuchKey'){
                    console.log("Callback.json doesn't exist, creating a new file");
@@ -87,22 +83,26 @@ exports.handler = function(event, context, callback) {
                    console.error("[Error: ] Unable to get objects at line 87", err);
                }
            }
-    
-           //--- Update content and upload to bucket ---
+
+           //  Update content and upload to bucket
            else{
-              console.log('Object data', data.Body);
+              console.log('Object data', data.Body.toString('ascii'));
               var content = data.Body.toString('ascii');
               content += JSON.stringify(fakedata) + ",";
+			  console.log("Content before upload", content.trim());
               uploadFile(content.trim());
            }
         });
     }
 
-    //-------------- UPLOAD A JSON FILE TO S3 -----------------
+	/**
+		@definition: this funciton writes a new .json file
+		@param {string} uploadContent: this string is written to the .json file
+	*/
     function uploadFile(uploadContent){
-        var tagobject = "claim=" + context.invokedFunctionArn; 
-        console.log("Tag object", tagobject); 
+        var tagobject = "claim=" + context.invokedFunctionArn;
         var base64data = new Buffer(JSON.stringify(uploadContent), 'binary');
+		console.log("Tag object", tagobject);
         s3.putObject({
             Bucket: bucketname,
             Key: bucketkey,
@@ -119,7 +119,9 @@ exports.handler = function(event, context, callback) {
         });
     }
 
-    //----------- CLEAR TAGS AFTER UPLOADING ------
+	/**
+		Def: clear tags of a bucket after Lambda is done writing
+	*/
     function clearTag(){
         var tagobj =  {
           TagSet: [{
@@ -130,7 +132,7 @@ exports.handler = function(event, context, callback) {
         bucketParams.Tagging = tagobj;
         s3.putObjectTagging(bucketParams, function(err, data){
             if (err){
-                console.log('[Error: ] Unable to tag objects');
+                console.log('[Error: ] Unable to tag objects', err);
             }
             else{
                 console.log('[Success: ] Clear bucket tags');
@@ -138,5 +140,73 @@ exports.handler = function(event, context, callback) {
         });
     }
 
+
+    /**
+		Def: delete content of an end point after it is done executing
+		@param {string} endpoint_to_delete: delete content with this endpoint
+	*/
+    function clearEndpoint(endpoint_to_delete){
+        var deleteParams = {
+            Bucket: bucketname,
+            Key: "doAtTrack/" + endpoint_to_delete + ".json"
+        };
+
+		// Read only the object at a given endpoint
+        s3.getObject(deleteParams, (err, data) => {
+            if (err)
+                console.error("Unable to get endpoint json", err);
+            else{
+                console.log('Object data', data.Body);
+                var content = data.Body.toString('ascii');
+                var res = parseString(content);
+
+				// If JSON is empty and delete JSON
+				if (content == ''){
+					s3.deleteObject(deleteParams, (err, data) => {
+						if (err) console.log(err, err.stack);
+						else console.log(data);
+					});
+				}
+
+				// If JSON is not empty, delete unexecuted calls
+                else{
+					var unexecutedStr = "";
+					res.forEach(call => {
+						if (call.executed != 'true'){
+							var callstr = JSON.stringify(call);
+							unexecutedStr += callstr;
+						}
+					});
+					uploadFile(unexecutedStr);
+				}
+            }
+        });
+    }
+
+
+	/**
+		@param {string} hello: the ascii string that will become an array of objects
+		@return {array} results: an array of calls both unexecuted and executed
+	*/
+    function parseString(hello){
+	      hello = hello.replace(/\\/g, '');
+	      hello = hello.substring(2, hello.length-1);
+	      var res = hello.split("}");
+	      var results = [];
+	      res.forEach(obj => {
+	        obj = obj + "}";
+	        obj = obj.substring(obj.indexOf("{"), obj.indexOf("}")+1);
+
+	        if (obj.charAt(0) == "{"){
+	          var json_obj = JSON.parse(obj);
+	          results.push(json_obj);
+	        }
+		});
+	      return results;
+    }
+
+
     getObjectTagging();
+    clearEndpoint("endpoint1");
+
 };
